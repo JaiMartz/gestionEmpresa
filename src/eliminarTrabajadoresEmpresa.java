2001d2cd8e23d490574d64d63c9ec008c9923159
Alvaro Millan <yunkpk@gmail.com>
2018-05-03 18:51:02 +0200

    Actualizacioin Alvaro

---
+++ src/eliminarTrabajadoresEmpresa.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author yunk-
+ */
+//cometido sea el de borrar aquellos empleados cuyo sueldo sea mayor que 2000 € de la lista de empleados.
+/*se deben ir recorriendo los ArrayList de Empleados y determinar aquellos que ganan un salario mayor a 2000 €, 
+y en caso de que ganen más de 2000€ eliminar dicho empleado de listaEmpleados.
+Por último se debe mostrar el núimero de Empleados que hay en cada departamento una vez se hayan realizado los borrados.*/
+
+import java.util.*;
+
+public class eliminarTrabajadoresEmpresa  {
+    
+ 
+        
+    }
+}
+    
+}
